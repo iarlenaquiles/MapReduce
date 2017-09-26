ba96e1e6183a221c9bdffc0e03b134addbeea97d
Iarlen <iarlensilva775@gmail.com>
2017-09-26 11:42:51 -0300

    Criando um WordCount

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.br.quixada.f;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
